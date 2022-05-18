662c077b0847c10381977642b845e33abfbcdfdf
bashyr-ali <btbashir@gmail.com>
2022-05-18 19:04:17 +0600

    init

---
+++ backend/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController:ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello world";
+        }
+    }
+}
